98bba5560183bd77a9bd3143905e34b758678bd4
Padma Patil <abdrazakovermek9@gmail.com>
2023-12-18 15:38:38 -0800

    Add function to index.js

--- src/index.js
+++ src/index.js
@@ -2,3 +2,28 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Calculator from './calculator.js';
+
+function reset() {
+    document.getElementById("form").reset();
+}
+
+function handleForm(e) {
+    e.preventDefault();
+    const inputYear = document.getElementById("year").value;
+    const inputMonth = document.getElementById("month").value;
+    const inputDay = document.getElementById("day").value;
+    const output = document.getElementById("output");
+    if (inputYear && inputMonth && inputDay) {
+        const calculateDay = new Calculator();
+        output.innerText = calculateDay.calculate(
+            inputYear,
+            inputMonth,
+            inputDay
+        );
+        reset();
+    } else {
+        output.innerText = "No date to calculate";
+    }
+}
+
+document.getElementById("form").addEventListener("submit", handleForm);
