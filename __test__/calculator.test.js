c7fdd31cc25e21252ba25c9bd8dfd5dfac7705e6
Padma Patil <abdrazakovermek9@gmail.com>
2023-12-18 22:32:44 -0800

    Second test pass

--- __test__/calculator.test.js
+++ __test__/calculator.test.js
@@ -4,5 +4,10 @@ describe('Calculator', () => {
     test('should correctly calculate day of the week for a specific date', () => {
         const calculateDay = new Calculator();
         expect(calculateDay.calculate(2023, 12, 18)).toMatch('Monday');
-    })
-})
+    });
+
+    test('should not calculate when date is not valid', () => {
+        const calculateDay = new Calculator();
+        expect(calculateDay.calculate(2023, 11, 31)).toBe('Cannot calculate: INVALID DATE');
+    });
+});
--- src/calculator.js
+++ src/calculator.js
@@ -11,7 +11,21 @@ export default class Calculator {
         ];
     }
 
+
+    isValidDate(year, month, day) {
+        const inputValue = new Date(year, month - 1, day);
+        return (
+            inputValue.getFullYear() === year &&
+            inputValue.getMonth() === month - 1 &&
+            inputValue.getDate() === day
+        );
+    }
+
     calculate(year, month, day) {
+        if (!this.isValidDate(year, month, day)) {
+            return 'Cannot calculate: INVALID DATE';
+        }
+
         const inputValue = new Date(year, month - 1, day);
 
         const dayOfWeek = this.dayOfWeek[inputValue.getDay()];
